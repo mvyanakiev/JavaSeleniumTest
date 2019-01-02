05a3d1bf5f003b54410f61a7045b397417e6732c
Milko Yanakiev <myanakiev@gmail.com>
2019-01-02 17:20:00 +0200

    frames

---
+++ src/test/java/com/swtestacademy/webdriver/FrameIndexTest.java
@@ -0,0 +1,27 @@
+package com.swtestacademy.webdriver;
+
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FrameIndexTest {
+
+    static WebDriver driver;
+    static Boolean osWin = false;
+
+    @BeforeClass
+    public static void setupTest() {
+
+        if (System.getProperty("os.name").startsWith("Win")) {
+            osWin = true;
+        }
+
+        if (osWin)
+            System.setProperty("webdriver.chrome.driver", ConstantsTests.CHROMEDRIVER_PATH);
+        driver = new ChromeDriver();
+    }
+
+
+
+
+}
