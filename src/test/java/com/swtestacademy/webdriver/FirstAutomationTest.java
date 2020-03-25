5b8209894bf376657959766ba03be6716eafbcfb
Milko Yanakiev <myanakiev@hotmail.com>
2020-03-25 13:29:08 +0200

    some fixes

--- src/test/java/com/swtestacademy/webdriver/FirstAutomationTest.java
+++ src/test/java/com/swtestacademy/webdriver/FirstAutomationTest.java
@@ -2,21 +2,25 @@ package com.swtestacademy.webdriver;
 
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstAutomationTest {
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void setupTest() {
+        driver = SetupTest.setupDriver();
+    }
 
     @Test
     public void titleTest(){
 
-        System.setProperty("webdriver.chrome.driver", ConstantsTests.CHROMEDRIVER_PATH);
-
-        WebDriver driver = new ChromeDriver();
         driver.navigate().to("https://www.teknosa.com/");
 
-        Assert.assertEquals("Title check failed!", "Teknosa | Teknosa Alışveriş Sitesi - Herkes İçin Teknoloji", driver.getTitle());
+        Assert.assertEquals("Title check failed!", "Teknosa | Herkes İçin Teknoloji - Teknosa Alışveriş Sitesi", driver.getTitle());
 
         driver.close();
         driver.quit();
